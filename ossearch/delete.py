9813a1b086d5005bf7c129aa2c6798f80fa0949d
JeffJerseyCow <jeffjerseycow@gmail.com>
2019-07-12 15:06:53 +0100

    fixed none type directory bug

--- ossearch/delete.py
+++ ossearch/delete.py
@@ -31,8 +31,12 @@ def delete_main(args: Namespace) -> bool:
         return True
 
     # check directory exists
-    path = os.path.realpath(args.directory)
-
+    try:
+        path = os.path.realpath(args.directory)
+    except TypeError:
+        log.critical('Please provide directory to delete')
+        return False
+    
     # check root node exists
     r = Node(name=path, path=path)
     if not gt.set_root(r):
